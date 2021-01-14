890f15bcd0b6bbf3375ae24fb4816db34f6e9246
prabaj90 <prabaj90@gmail.com>
2021-01-14 22:55:38 +0530

    Empty Message Added

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -4,11 +4,16 @@ import {GlobalContext} from '../context/GlobalState';
 
 export const TransactionList = () => {
     const {transactions} = useContext(GlobalContext);
+    console.log(transactions.length);
     return (
         <>
             <h3>History</h3>
              <ul className="list">
-                {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction}/>))}
+                { transactions.length > 0 
+                ? transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction}/>))
+                : <li className='listEmpty'>Transaction List is Empty </li>
+                }
+            
             </ul>
         </>
     )
